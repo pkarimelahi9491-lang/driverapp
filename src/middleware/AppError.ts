d57556d502262f1c461ab523934446f94c9028e1
Parisa Karimelahi <p.karimelahi9491@gmail.com>
2026-09-02 16:40:30 +0330

    Update AppError.ts

--- src/middleware/AppError.ts
+++ src/middleware/AppError.ts
@@ -9,6 +9,30 @@ export class AppError extends Error {
 
     Error.captureStackTrace(this, this.constructor);
   }
+
+  static badRequest(message = 'درخواست نامعتبر است'): AppError {
+    return new AppError(message, 400);
+  }
+
+  static unauthorized(message = 'عدم احراز هویت'): AppError {
+    return new AppError(message, 401);
+  }
+
+  static forbidden(message = 'عدم دسترسی مجاز'): AppError {
+    return new AppError(message, 403);
+  }
+
+  static notFound(message = 'موردی یافت نشد'): AppError {
+    return new AppError(message, 404);
+  }
+
+  static conflict(message = 'تداخل در داده‌ها رخ داده است'): AppError {
+    return new AppError(message, 409);
+  }
+
+  static internal(message = 'خطای داخلی سرور'): AppError {
+    return new AppError(message, 500);
+  }
 }
 
 export default AppError;
